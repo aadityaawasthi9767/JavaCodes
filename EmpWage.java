68ef6657f9e18973958942bb983dee8ac449730e
Aaditya Awasthi <aadityaawasyhi97672gmail.com>
2020-06-11 20:16:57 +0530

    Updated: Employee Attendance

--- EmpWage.java
+++ EmpWage.java
@@ -1,5 +1,24 @@
+import java.util.Random;
+
 public class EmpWage{
-	public static void main(String args[]){
-		System.out.println("Welcome to Employee Wage Program in JAVA");
+
+void AttendanceFunc(){
+
+	Random ran = new Random();
+
+	int empCheck = ran.nextInt(2);
+	System.out.print(empCheck);
+	if(empCheck==0)
+		System.out.println("Present");
+	else
+		System.out.println("Absent");
+}
+
+public static void main(String args[]){
+
+System.out.println("Welcome to Employee Wage Program in JAVA");
+EmpWage emp = new EmpWage();
+emp.AttendanceFunc();
+
 	}
 }
