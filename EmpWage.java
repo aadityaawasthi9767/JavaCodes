1d4155b693541b7a254271a9633743971ec79d55
Aaditya Awasthi <aadityaawasyhi97672gmail.com>
2020-06-12 01:07:28 +0530

    Updated: Employee Wage per Day

--- EmpWage.java
+++ EmpWage.java
@@ -2,23 +2,33 @@ import java.util.Random;
 
 public class EmpWage{
 
+int empCheck, WageperHr=20, Fullday=8;
+float DWage=0;
 void AttendanceFunc(){
 
 	Random ran = new Random();
 
-	int empCheck = ran.nextInt(2);
-	System.out.print(empCheck);
+	empCheck = ran.nextInt(2);
 	if(empCheck==0)
 		System.out.println("Present");
 	else
 		System.out.println("Absent");
 }
 
+void DailyWage(){
+	if(empCheck==0){
+		DWage=Fullday*WageperHr;
+		System.out.println("Daily Wage: "+DWage);
+	}
+	else
+		System.out.println("Daily Wage: "+DWage);
+	}
+
 public static void main(String args[]){
 
 System.out.println("Welcome to Employee Wage Program in JAVA");
 EmpWage emp = new EmpWage();
 emp.AttendanceFunc();
-
+emp.DailyWage();
 	}
 }
