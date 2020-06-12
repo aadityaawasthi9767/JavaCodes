82ccffa1cf17542ff806c3e7cf25e33844fb9093
Aaditya Awasthi <aadityaawasyhi97672gmail.com>
2020-06-12 21:10:10 +0530

    Updated: Added the Switch Case

--- EmpWage.java
+++ EmpWage.java
@@ -2,38 +2,37 @@ import java.util.Random;
 
 public class EmpWage{
 
-int empCheck, WageperHr=20, Fullday=8, Halfday=4;
-float DWage=0;
-void AttendanceFunc(){
-
-	Random ran = new Random();
-
-	empCheck = ran.nextInt(3);
-	if(empCheck==0)
-		System.out.println("Present, FULL DAY!");
-	else if(empCheck==1)
-		System.out.println("Absent");
-	else
-		System.out.println("Present, HALF DAY!");
-}
-
-void DailyWage(){
-	if(empCheck==0){
-		DWage=Fullday*WageperHr;
+void DailyWage(int a, int b){
+		DWage=a*b;
 		System.out.println("Daily Wage: "+DWage);
 	}
-	else if(empCheck==1)
-		System.out.println("Daily Wage: "+DWage);
-	else{
-		DWage=Halfday*WageperHr;
-		System.out.println("Daily Wage: "+DWage);
-	}
-}
+
 public static void main(String args[]){
 
+int empCheck, WageperHr=20, Fullday=8, Halfday=4;
+float DWage=0;
+
+EmpWage emp= new EmpWage();
 System.out.println("Welcome to Employee Wage Program in JAVA");
-EmpWage emp = new EmpWage();
-emp.AttendanceFunc();
-emp.DailyWage();
+Random ran = new Random();
+empCheck = ran.nextInt(3);
+
+switch(empCheck){
+	case 0:
+			System.out.println("Present, FULL DAY!");
+			emp.DailyWage(Fullday,WageperHr);
+			break;
+
+	case 1:
+			System.out.println("Present, HALF DAY!");
+			emp.DailyWage(Halfday,WageperHr);
+			break;
+
+	case 2:
+			System.out.println("Absent! Sorry, NO WAGE TODAY.");
+			break;
+default:
+			System.out.println("Not a Valid Entry!");
+		}
 	}
 }
