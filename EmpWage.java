b67d93d78b7944c40c1b14f14d9bb230e2d70334
Aaditya Awasthi <aadityaawasyhi97672gmail.com>
2020-06-20 02:06:44 +0530

    Updated: Multiple componies

--- EmpWage.java
+++ EmpWage.java
@@ -1,9 +1,20 @@
 import java.util.Random;
-
+//import java.util.String;
 public class EmpWage
 {
+public static int[] WageRecords=new int[20];
 public static final int IS_FULL_TIME=1;
 public static final int IS_HALF_TIME=2;
+public static String comName;
+public static int empRate,numofDays,MaxHoursMonth;
+	EmpWage(String comName, int empRate, int numofDays, int MaxHoursMonth){
+
+	this.comName=comName;
+	this.empRate=empRate;
+	this.numofDays=numofDays;
+	this.MaxHoursMonth=MaxHoursMonth;
+
+	}
 
 	int DailyWage(String comName, int empRate, int numOfDays, int maxHoursMonth){
 
@@ -36,28 +47,39 @@ public static final int IS_HALF_TIME=2;
 			System.out.println("Day "+totalWorkingDays+ "Emp Hr: "+empHrs);
 		}
 		int totalEmpWage=totalEmpHrs*empRate;
+		for(int i=0;i<19;i++)
+			{
+				int counter=0;
+				WageRecords[counter]=totalEmpWage;
+				counter++;
+			}
+
+
 System.out.print("Total Emp Wage for company: " +comName+"="+totalEmpWage);
 return totalEmpWage;
 	}
 
 	/*void displayWage(){
 
-		for(int i=0;i<WageRecords.length;i++)
+		for(int i=0 ; i< WageRecords.lenght() ; i++)
 		{
-			System.out.println("Today's Wage:"+WageRecords[i]);
-			sum=sum+WageRecords[i];
+			System.out.println("Today's Wage for"+comName+"is: "+WageRecords[i]);
+			
 		}
-			System.out.println("Monthy Salary: "+sum);
 	}*/
 
 
 public static void main(String args[])
 {
-
-EmpWage emp= new EmpWage();
 System.out.println("Welcome to Employee Wage Program in JAVA");
+EmpWage emp1= new EmpWage("DMart",20,20,100);
+EmpWage emp2= new EmpWage("AllFreash",50,25,200);
+//System.out.println("Welcome to Employee Wage Program in JAVA");
 
-emp.DailyWage("DMart",20,20,10);
-emp.DailyWage("AllFresh",50,25,20);
+emp1.DailyWage("DMart",20,20,100);
+emp2.DailyWage("AllFresh",50,25,200);
+//emp1.displayEmpWage();
+//emp2.displayEmpWage();
 }
+
 }
